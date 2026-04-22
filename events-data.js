c69537bcad0fdc198cc93451eafac98b08583079
GalgenbergAalen <275723028+GalgenbergAalen@users.noreply.github.com>
2026-04-22 15:53:36 +0200

    Update events-data.js

--- events-data.js
+++ events-data.js
@@ -30,8 +30,8 @@ const events = [
     location: "Prof.-Wagner-Platz",
     description: "Offenes Nachbarschaftstreffen zum Kennenlernen und Austauschen.",
     hint: "Nur bei sehr schlechtem Wetter ggf. kurzfristige Änderung.",
-    isCancelled: true,
-    cancelNote: "Dies ist ein Test"
+    isCancelled: false,
+    cancelNote: "Wetterbedingt"
   },
   {
     date: "2026-06-12",
@@ -46,7 +46,7 @@ const events = [
     description: "Offenes Nachbarschaftstreffen zum Kennenlernen und Austauschen.",
     hint: "Nur bei sehr schlechtem Wetter ggf. kurzfristige Änderung.",
     isCancelled: false,
-    cancelNote: ""
+    cancelNote: "Wetterbedingt"
   },
   {
     date: "2026-07-10",
@@ -61,7 +61,7 @@ const events = [
     description: "Offenes Nachbarschaftstreffen zum Kennenlernen und Austauschen.",
     hint: "Nur bei sehr schlechtem Wetter ggf. kurzfristige Änderung.",
     isCancelled: false,
-    cancelNote: ""
+    cancelNote: "Wetterbedingt"
   },
   {
     date: "2026-08-14",
@@ -76,7 +76,7 @@ const events = [
     description: "Offenes Nachbarschaftstreffen zum Kennenlernen und Austauschen.",
     hint: "Nur bei sehr schlechtem Wetter ggf. kurzfristige Änderung.",
     isCancelled: false,
-    cancelNote: ""
+    cancelNote: "Wetterbedingt"
   },
   {
     date: "2026-09-18",
@@ -91,7 +91,7 @@ const events = [
     description: "Offenes Nachbarschaftstreffen zum Kennenlernen und Austauschen.",
     hint: "Nur bei sehr schlechtem Wetter ggf. kurzfristige Änderung.",
     isCancelled: false,
-    cancelNote: ""
+    cancelNote: "Wetterbedingt"
   },
   {
     date: "2026-10-09",
@@ -106,6 +106,6 @@ const events = [
     description: "Offenes Nachbarschaftstreffen zum Kennenlernen und Austauschen.",
     hint: "Nur bei sehr schlechtem Wetter ggf. kurzfristige Änderung.",
     isCancelled: false,
-    cancelNote: ""
+    cancelNote: "Wetterbedingt"
   }
 ];
