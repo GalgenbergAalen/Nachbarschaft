b3f9f2f83ddbec084fc46272d40fe64268a755e7
GalgenbergAalen <275723028+GalgenbergAalen@users.noreply.github.com>
2026-06-18 16:35:11 +0200

    Update infos-data.js

--- infos-data.js
+++ infos-data.js
@@ -5,31 +5,13 @@
 const infos = [
   {
     date: "2026-06-03",
-    category: "Eröffnungen",
     title: "Freibadbereich im Hirschbachbad eröffnet",
     shortText: "Mit dem neuen Freibadbereich im Hirschbachbad startet die Freibadsaison wieder mit drei Freibädern in Aalen.",
     location: "Hirschbachbad, Aalen",
     link: "https://www.aalen.de/freibadsaison-startet-mit-drei-freibaedern.249260.25.htm"
   },
   {
-    date: "2025-12-09",
-    category: "Eröffnungen",
-    title: "Rad- und Fußweg „Schafgärten“ eröffnet",
-    shortText: "Der neue Weg schafft eine schnelle und sichere Verbindung zur Innenstadt und verbessert auch den Schulweg zum Galgenberg.",
-    location: "Schafgärten, Aalen",
-    link: "https://www.aalen.de/eroeffnung-rad-und-fussweg-schafgaerten.242201.25.htm"
-  },
-  {
-    date: "2025-11-28",
-    category: "Eröffnungen",
-    title: "Hallenbad im Hirschbach eröffnet",
-    shortText: "Das neue Hallenbad im Hirschbach wurde feierlich eröffnet und ergänzt seitdem das Badangebot in Aalen.",
-    location: "Hirschbachbad, Aalen",
-    link: "https://www.aalen.de/hirschbachbad-ist-eroeffnet.241851.25.htm"
-  },
-  {
     date: "2026-06-03",
-    category: "Laufende und anstehende Bauprojekte",
     title: "Erdarbeiten für Quartiersgarage im Tannenwäldle",
     shortText: "Im Baugebiet Tannenwäldle laufen die Arbeiten für die Quartiersgarage und weitere Wohnungen weiter.",
     location: "Tannenwäldle / Ziegelstraße, Aalen",
@@ -37,7 +19,6 @@ const infos = [
   },
   {
     date: "2026-03-04",
-    category: "Laufende und anstehende Bauprojekte",
     title: "Baustart für das Wohnprojekt Ziegelstraße 20",
     shortText: "Mit dem Spatenstich hat der offizielle Bau des neuen Mehrfamilienhauses mit 14 Wohnungen begonnen.",
     location: "Ziegelstraße 20, Aalen",
@@ -45,15 +26,27 @@ const infos = [
   },
   {
     date: "2025-12-09",
-    category: "Laufende und anstehende Bauprojekte",
+    title: "Rad- und Fußweg „Schafgärten“ eröffnet",
+    shortText: "Der neue Weg schafft eine schnelle und sichere Verbindung zur Innenstadt und verbessert auch den Schulweg zum Galgenberg.",
+    location: "Schafgärten, Aalen",
+    link: "https://www.aalen.de/eroeffnung-rad-und-fussweg-schafgaerten.242201.25.htm"
+  },
+  {
+    date: "2025-12-09",
     title: "Spatenstich für Quartiersgarage und 56 Wohnungen im Tannenwäldle",
     shortText: "Im Tannenwäldle wurde der Bau einer Quartiersgarage mit 224 Stellplätzen und weiterer Wohnungen gestartet.",
     location: "Baugebiet Tannenwäldle, Aalen",
     link: "https://www.aalen.de/im-baugebiet-tannenwaeldle-gehts-voran.242198.25.htm"
   },
   {
+    date: "2025-11-28",
+    title: "Hallenbad im Hirschbach eröffnet",
+    shortText: "Das neue Hallenbad im Hirschbach wurde feierlich eröffnet und ergänzt seitdem das Badangebot in Aalen.",
+    location: "Hirschbachbad, Aalen",
+    link: "https://www.aalen.de/hirschbachbad-ist-eroeffnet.241851.25.htm"
+  },
+  {
     date: "2023-12-20",
-    category: "Laufende und anstehende Bauprojekte",
     title: "Kreisverkehr an Galgenbergstraße / Hirschbachstraße",
     shortText: "Im städtischen Baustellenplan wird der Bau eines neuen Kreisverkehrs an der Ecke Galgenbergstraße / Hirschbachstraße als laufende Maßnahme geführt.",
     location: "Galgenbergstraße / Hirschbachstraße, Aalen",
@@ -61,7 +54,6 @@ const infos = [
   },
   {
     date: "2023-05-25",
-    category: "Aktuelle Infos aus dem Viertel",
     title: "Neues Infopult am Wagnerplatz",
     shortText: "Am Wagnerplatz wurde ein neues Infopult zum Wohnquartier im Rahmen eines historischen Stadtspaziergangs vorgestellt.",
     location: "Wagnerplatz, Aalen",
